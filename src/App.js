b97268301fbbcb8d6d09f86b329f75205cc7b08c
Andre Araujo <andreearaujo98@gmail.com>
2020-12-10 16:14:13 -0300

    youtube main page finalized

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+import { ThemeProvider, createMuiTheme, makeStyles} from '@material-ui/core';
+
+import Home from './Home';
 
 function App() {
-  return <div></div>;
+  const [darkMode, setDarkMode] = useState(false);
+
+  const theme = createMuiTheme({
+    palette: {
+      type: darkMode ? 'dark' : 'light',
+      primary: {
+        main: '#f44336'
+      },
+      secondary: {
+        main: '#3EA6FF'
+      },
+      background: {
+        default: darkMode ? '#232323' : '#FFF',
+        dark: darkMode ? '#181818' : 'f4f6f8',
+        paper: darkMode ? '#232323' : '#FFF'
+      }
+    }
+  });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Home darkMode={darkMode} setDarkMode={setDarkMode}/>
+    </ThemeProvider>
+    
+  );
 }
 
 export default App;
